69d14979bf5982e1422af132c0f504c14b856438
Abdulsalam <habeebabdulsalam86@gmail.com>
2025-07-23 12:45:39 +0100

    accepted images from an array

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -23,6 +23,10 @@ class Product extends Model
         });
     }
 
+    protected $casts = [
+        'images' => 'array',
+    ];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
